fe4da847203863e25be6be08dc09c61b206814e7
bohndezdev <bohndez.dev@gmail.com>
2020-11-28 18:47:17 -0300

    Add | Changing params on the proof params

--- src/js/second.js
+++ src/js/second.js
@@ -1,4 +1,4 @@
 const numbers2 = [11,22,33]
 const numbersTriple = numbers.map(number =>number * 6)
 
-console.log('HOLA')
+console.log('HOLAAAA')
--- src/ts/person.ts
+++ src/ts/person.ts
@@ -1,6 +1,7 @@
 export class Person {
   name: string
   lastNam: number
+  address: string
   constructor(name: string) {
     this.name = name
   }
